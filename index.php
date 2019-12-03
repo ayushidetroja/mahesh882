4bf7d0e250aa134b15c03feb378e6d6941d58cef
ayushidetroja <49086655+ayushidetroja@users.noreply.github.com>
2019-12-03 11:38:07 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Image</title>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<style>
+	.subscriber-main {
+	    margin-top: 50px;
+	}
+	.subscriberImg img {
+	    border-radius: 50%;
+	    width: 230px;
+	    height: 230px;
+	    margin: 0 auto;
+	}
+	.subscriber_text {
+	    text-align: center;
+	    font-size: 24px;
+	    color: #000;
+	}
+	.subscriberimg-section {
+	    background-color: #f5f5f5;
+	    padding: 30px 0px;
+	}
+</style>
+<body>
+<div class="container">
+<div class="row subscriber-main">
+	<div class="col-md-4">
+	<div class="subscriberimg-section">
+	<div class="subscriberImg">
+		<img class="card-img-top img-responsive " src="http://propeller.in/assets/images/profile-pic.png" alt="Card image cap">
+	</div>
+    <div class="card-body">
+		<h2 class="card-title subscriber_text">Card title</h2>
+	</div>
+	</div>
+	</div>
+
+	<div class="col-md-4">
+	<div class="subscriberimg-section">
+	<div class="subscriberImg">
+		<img class="card-img-top img-responsive " src="http://propeller.in/assets/images/profile-pic.png" alt="Card image cap">
+	</div>
+    <div class="card-body">
+		<h2 class="card-title subscriber_text">Card title</h2>
+	</div>
+	</div>
+	</div>
+
+	<div class="col-md-4">
+	<div class="subscriberimg-section">
+	<div class="subscriberImg">
+		<img class="card-img-top img-responsive " src="http://propeller.in/assets/images/profile-pic.png" alt="Card image cap">
+	</div>
+    <div class="card-body">
+		<h2 class="card-title subscriber_text">Card title</h2>
+	</div>
+	</div>
+	</div>
+
+	</div>
+</div>
+</body>
+</html>
